4991393504e826016686fe85cc16ecbfbf318c0f
backendVirtuoso <129765568+backendVirtuoso@users.noreply.github.com>
2026-02-20 16:27:34 +0900

    [실습] Jenkins 연동을 위한 메인 메소드 및 환경설정 수정

--- src/main/java/com/system/batch/settlement/SettlementApplication.java
+++ src/main/java/com/system/batch/settlement/SettlementApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
+//@EnableScheduling
 @SpringBootApplication
 public class SettlementApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SettlementApplication.class, args);
+        System.exit(SpringApplication.exit(SpringApplication.run(SettlementApplication.class, args)));
     }
 
 }
